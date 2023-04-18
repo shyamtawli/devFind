149e104342004e8d928ce867a98e7faa116586d1
shyamtawli <shyamtawli2@gmail.com>
2023-04-18 20:23:58 +0530

    unnecessary clean up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">devFind</div>;
 }
 
 export default App;
