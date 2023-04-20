812ccca32fc516f8d405cfa04652389d506fa952
shyamtawli <shyamtawli2@gmail.com>
2023-04-20 20:40:09 +0530

    search bar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Search from "./components/Search/Search";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <Search />
     </div>
   );
 }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Search.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function Search() {
-  return <div>Search</div>;
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Search..." />
+      <FontAwesomeIcon
+        className="search-icon"
+        icon={faMagnifyingGlass}
+        style={{ color: "#fff" }}
+      />
+    </div>
+  );
 }
 
 export default Search;
