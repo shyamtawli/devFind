3b729efa1006c83bc5324b9655879bc37878cd60
shyamtawli <shyamtawli2@gmail.com>
2023-04-23 00:27:21 +0530

    data from JSON file and style fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
 import Profile from "./components/Profile/Profile";
 import Search from "./components/Search/Search";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
+  const [developers, setDevelopers] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("../data/shyamtawli.json");
+        const data = await response.json();
+        setDevelopers(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(developers);
+
   return (
     <div className="App">
       <Sidebar />
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,92 @@
 import React from "react";
 import "./Profile.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitterSquare,
+  faLinkedin,
+  faGithubSquare,
+} from "@fortawesome/free-brands-svg-icons";
+import data from "../../data/shyamtawli.json";
 
 function Profile() {
   return (
     <div className="profile-container">
-      <Card />
+      <Card data={data} />
     </div>
   );
 
-  function Card() {
+  function Card({ data }) {
     return (
       <div className="profile-card">
         <div className="top-container">
           <div className="profile-photo">
-            <img src={`https://github.com/shyamtawli.png`} alt="User logo" />
+            <img src={data.avatar} alt="User logo" />
           </div>
           <div className="profile-details">
-            <h3>Shyam Tawli</h3>
-            <p>Indore, India</p>
+            <h3>{data.name}</h3>
+            <p>
+              <span style={{ marginRight: "0.5rem" }}>
+                <FontAwesomeIcon icon={faLocationDot} />
+              </span>
+              {data.location}
+            </p>
             <div className="skills-container">
-              <div className="skill">NodeJS</div>
-              <div className="skill">HTML</div>
-              <div className="skill">CSS</div>
-              <div className="skill">ReactJS</div>
+              {data.skills.map((skill) => {
+                return <div className="skill">{skill}</div>;
+              })}
             </div>
           </div>
-          <div className="profile-link">View Profile &#8594;</div>
+          <div className="profile-link">
+            <a href={data.portfolio}>View Profile &#8594;</a>
+          </div>
+        </div>
+        <div className="bottom-container">
+          <div className="about">{data.bio}</div>
+          <div className="social">
+            <div className="github">
+              <FontAwesomeIcon
+                icon={faGithubSquare}
+                size="xl"
+                style={{ color: "#00a6fb" }}
+              />
+              <a
+                className="social-link"
+                href={data.social.GitHub}
+                target="_blank"
+              >
+                {data.social.GitHub.replace("https://github.com/", "")}
+              </a>
+            </div>
+            <div className="twitter">
+              <FontAwesomeIcon
+                icon={faTwitterSquare}
+                size="xl"
+                style={{ color: "#00a6fb" }}
+              />
+              <a
+                className="social-link"
+                href={data.social.Twitter}
+                target="_blank"
+              >
+                {data.social.Twitter.replace("https://twitter.com/", "")}
+              </a>
+            </div>
+            <div className="linkedin">
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                size="xl"
+                style={{ color: "#00a6fb" }}
+              />
+              <a
+                className="social-link"
+                href={data.social.LinkedIn}
+                target="_blank"
+              >
+                {data.social.LinkedIn.replace("https://linkedin.com/in/", "")}
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     );
