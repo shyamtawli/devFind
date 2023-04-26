793af1524c9583c16d098c804dd5f41a1767771f
Munish Pathania <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>
2023-04-26 23:34:50 +0530

    updatedSearch.jsx

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,26 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./Search.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function Search({ onSearch }) {
   const [searchValue, setSearchValue] = useState("");
+  const [prevSearchValue, setPrevSearchValue] = useState("");
+  const searchInput = useRef(null);
 
   const handleInputChange = (event) => {
     setSearchValue(event.target.value);
   };
 
   const handleSearch = () => {
-    onSearch(searchValue);
+    if (searchValue !== "" && searchValue !== prevSearchValue) {
+      onSearch(searchValue);
+      setPrevSearchValue(searchValue);
+    }
   };
+
   const handleSearchOnEnter = (e) => {
     if (e.keyCode === 13) {
       handleSearch();
     }
   };
+
+  // Focus the search input when the component mounts
+  useEffect(() => {
+    searchInput.current.focus();
+  }, []);
+
   return (
     <div className="search-bar">
       <input
+        ref={searchInput}
         type="text"
         onChange={handleInputChange}
         value={searchValue}
