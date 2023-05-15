4396bfa9a03552da1b688aa62f95acb53f5b561a
Hayat ilyas <ihayat855@gmail.com>
2023-05-15 15:53:27 +0530

    improve the testprofile page code

--- src/Pages/TestProfile.jsx
+++ src/Pages/TestProfile.jsx
@@ -1,39 +1,42 @@
 import React, { Fragment, Suspense, lazy, useEffect, useState } from "react";
-import HayatProfile from "../data/HayatProfile.json";
-import ShayamProfile from "../data/ShayamProfile.json";
-import SabaProfile from "../data/SabaProfile.json";
-import VinayProfile from "../data/VinayProfile.json";
-import RanaProfile from "../data/RanaProfile.json";
 const Sidebar = lazy(() => import("../components/Sidebar/Sidebar"));
 const ProfileComponent = lazy(() => import("../components/Profile/Profile"));
 
 export default function TestProfile() {
   const [profile, setprofile] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const fileListResponse = await fetch(`/data/ProfileList.json`);
+      const fileList = await fileListResponse.json();
+
+      const ProfileDataPromise = fileList.map(async (filename) => {
+        const profileResponse = await fetch(`/data/${filename}`);
+        const data = await profileResponse.json();
+        return data;
+      });
+
+      const AllProfileDataSetteled = await Promise.all(ProfileDataPromise);
+      setprofile(AllProfileDataSetteled);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    const data_arrays = [
-      HayatProfile,
-      ShayamProfile,
-      RanaProfile,
-      VinayProfile,
-      SabaProfile,
-    ];
-    const MergedArray = data_arrays.flat();
-    setprofile(MergedArray);
+    fetchData();
   }, []);
+
   return (
     <Fragment>
-      <Suspense fallback={<p style={{color:'white'}}>loading..</p>}>
+      <Suspense fallback={<p style={{ color: "white" }}>loading..</p>}>
         <Sidebar />
       </Suspense>
-        {profile.length > 1
-          ? profile.map((item, index) => {
-              return (
-                <Suspense fallback={<p style={{color:'white'}}>loading...</p>}>
-                  <ProfileComponent data={item}  />
-                </Suspense>
-              );
-            })
-          : null}
+      {profile.length > 1
+        ? profile.map((data, index) => (
+            <ProfileComponent data={data} key={index} />
+          ))
+        : null}
     </Fragment>
   );
 }
