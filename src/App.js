71b430294ee7bfe34008af9dbfbd6b7872e75b56
ThienBNguyen <thien.nguyen30191@gmail.com>
2023-05-15 20:15:35 -0500

    Fix: Improve Search Filter #142

--- src/App.js
+++ src/App.js
@@ -9,30 +9,24 @@ import "./App.css";
 function App() {
   const [profiles, setProfiles] = useState([]);
   const [searching, setSearching] = useState(false);
-
   const handleSearch = (searchValue) => {
     const lowercaseSearch = searchValue.toLowerCase();
-    const results = [];
-
-    for (const object of datas) {
+    setSearching(true);
+    const results = datas.filter((object) => {
       const lowercaseName = object.name.toLowerCase();
       const lowercaseLocation = object.location.toLowerCase();
       const matchingSkills = object.skills.filter((skill) =>
         skill.toLowerCase().includes(lowercaseSearch)
       );
-      if (
+      return (
         matchingSkills.length > 0 ||
         lowercaseName.includes(lowercaseSearch) ||
         lowercaseLocation.includes(lowercaseSearch)
-      ) {
-        results.push(object);
-      }
-    }
-
-    setSearching(true);
+      );
+    });
     setProfiles(results);
   };
-
+ 
   const shuffleProfiles = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -46,7 +40,7 @@ function App() {
   return (
     <div className="App">
       <Sidebar />
-      <Search onSearch={handleSearch}  suggestions={datas}/>
+      <Search onSearch={handleSearch} />
       {profiles.length === 0 && searching ? (
         <NoResultFound />
       ) : profiles.length === 0 && !searching ? (
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,15 +3,12 @@ import "./Search.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-function Search({ onSearch, suggestions }) {
+function Search({ onSearch }) {
   const [searchValue, setSearchValue] = useState("");
   const [prevSearchValue, setPrevSearchValue] = useState("");
   const searchInput = useRef(null);
-  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
-  const [showSuggestions, setShowSuggestions] = useState(false);
   const handleInputChange = (event) => {
     setSearchValue(event.target.value);
-    setShowSuggestions(true);
   };
 
   const handleSearch = () => {
@@ -22,35 +19,18 @@ function Search({ onSearch, suggestions }) {
   };
 
   const handleSearchOnEnter = (e) => {
+    onSearch(e.target.value)
     if (e.keyCode === 13) {
       handleSearch();
     }
   };
-
   const handleSearchButtonClick = () => {
     handleSearch();
   };
-
   useEffect(() => {
     searchInput.current.focus();
   }, []);
-  useEffect(() => {
-    setFilteredSuggestions(
-      suggestions.filter(
-        (suggestion) =>
-          suggestion.name.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 ||
-          suggestion.location.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 ||
-          suggestion.skills.some(skill =>
-            skill.toLowerCase().indexOf(searchValue.toLowerCase()) > -1
-          )
-      )
-    );
-  }, [searchValue, suggestions]);
-  const onSuggestionClicked = (suggestion) => {
-    onSearch(suggestion.name);
-    setSearchValue(suggestion.name);
-    setShowSuggestions(false);
-  };
+
 
   return (
     <div className="search-bar">
@@ -67,16 +47,7 @@ function Search({ onSearch, suggestions }) {
         className="search-icon"
         icon={faMagnifyingGlass}
       />
-      {showSuggestions && searchValue && (
-        <ul className="suggestion-list">
-          {filteredSuggestions.slice(0, 5).map((suggestion, i) => (
-            <li key={i} onClick={() => onSuggestionClicked(suggestion)}>
-              {suggestion.name} - {suggestion.location}
-            </li>
-          ))}
-      </ul>
-      )}
-      
+    
     </div>
   );
 }
