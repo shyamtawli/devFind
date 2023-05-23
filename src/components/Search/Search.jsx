086ff2bcaa70327418acab5687f27a1229f993ed
Bharath Kalyan S <sbharath35@gmail.com>
2023-05-23 10:51:51 +0530

    feat: Adds the feature for issue #199

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -10,6 +10,7 @@ function Search({ onSearch }) {
 
   const handleInputChange = (event) => {
     setSearchValue(event.target.value);
+    handleSearch();
   };
 
   const handleSearch = () => {
@@ -34,21 +35,21 @@ function Search({ onSearch }) {
   }, []);
 
   return (
-        <div className="search-bar">
-          <input
-              ref={searchInput}
-              type="text"
-              onChange={handleInputChange}
-              value={searchValue}
-              placeholder="Search user by name, location or skills"
-              onKeyDown={handleSearchOnEnter}
-          />
-          <FontAwesomeIcon
-              onClick={handleSearchButtonClick}
-              className="search-icon"
-              icon={faMagnifyingGlass}
-          />
-        </div>
+    <div className="search-bar">
+      <input
+        ref={searchInput}
+        type="text"
+        onChange={handleInputChange}
+        value={searchValue}
+        placeholder="Search user by name, location or skills"
+        onKeyDown={handleSearchOnEnter}
+      />
+      <FontAwesomeIcon
+        onClick={handleSearchButtonClick}
+        className="search-icon"
+        icon={faMagnifyingGlass}
+      />
+    </div>
   );
 }
 
