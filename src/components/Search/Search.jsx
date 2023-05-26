325f1126f6ffa3eff1571f614ce6139abdc5663d
Prajwal Aradhya S K <manuaradhya07@gmail.com>
2023-05-26 19:35:41 +0530

    added debounced feature to search efficiently

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,46 +2,45 @@ import React, { useState, useRef, useEffect } from "react";
 import "./Search.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useDebouncedState } from "../hooks/useDebouncedState";
 
 function Search({ onSearch }) {
   const [searchValue, setSearchValue] = useState("");
-  const [prevSearchValue, setPrevSearchValue] = useState("");
   const searchInput = useRef(null);
-
-  const handleInputChange = (event) => {
-    setSearchValue(event.target.value);
-  };
-
-  const handleSearch = () => {
-    if (searchValue !== prevSearchValue) {
-      onSearch(searchValue);
-      setPrevSearchValue(searchValue);
-    }
-  };
+  const [debouncedValue, setDebouncedValue] = useDebouncedState('', 450);
 
   const handleSearchOnEnter = (e) => {
-    if (e.keyCode === 13) {
-      handleSearch();
-    }
+    const value = e.currentTarget.value;
+    setDebouncedValue(value);
   };
 
   const handleSearchButtonClick = () => {
-    handleSearch();
+    setDebouncedValue(debouncedValue);
+  };
+
+  const handleOnTextChange = (e) => {
+    const value = e.currentTarget.value;
+    setSearchValue(value);
+    setDebouncedValue(value);
   };
 
   useEffect(() => {
     searchInput.current.focus();
   }, []);
 
+  useEffect(() => {
+    onSearch(debouncedValue);
+  }, [debouncedValue]);
+
   return (
         <div className="search-bar">
           <input
               ref={searchInput}
               type="text"
-              onChange={handleInputChange}
+              onChange={handleOnTextChange}
               value={searchValue}
               placeholder="Search user by name, location or skills"
-              onKeyDown={handleSearchOnEnter}
+              onKeyUp={handleSearchOnEnter}
           />
           <FontAwesomeIcon
               onClick={handleSearchButtonClick}
