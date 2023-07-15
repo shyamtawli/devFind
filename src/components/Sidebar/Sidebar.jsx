aeacc470deab760d4dcee587936e684b8ba45d7e
Seifeldin Abdelhamid <100273762+seifhamdy@users.noreply.github.com>
2023-07-15 17:33:47 +0300

    FIX: Responsiveness

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -20,7 +20,7 @@ function Sidebar() {
       <div className="headline">Discover and Connect with Skilled Developers.</div>
       <div className="description">
         <a href=" https://github.com/shyamtawli/devFind#how-to-add-your-profile-" target="_blank" rel="noreferrer">
-          Add your profile?
+          Add your profile
         </a>
       </div>
     </div>
