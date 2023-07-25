37bb6dead07853862708c1e2263539fdfe7f04b6
feelthatvib3 <88098862+feelthatvib3@users.noreply.github.com>
2023-07-25 20:52:36 +0200

    Change classNames' names and structure imports

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import './Sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCode } from '@fortawesome/free-solid-svg-icons';
+
+import './Sidebar.css';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="logo-title">
-        <div className="logo">
-          <FontAwesomeIcon icon={faCode} size="2xl" style={{ color: '#ffffff' }} />
+      <div className="sidebar-container">
+        <div className="sidebar-logo">
+          <FontAwesomeIcon icon={faCode} size="xl" />
+          <a href="https://dev-find.vercel.app/" className="sidebar-title">
+            <span>dev</span>
+            <span className="highlighted">Find</span>
+          </a>
         </div>
-        <a href="https://dev-find.vercel.app/" className="titlelink">
-          <div className="title">
-            <p className="dev">dev</p>
-            <p className="find">Find</p>
-          </div>
-        </a>
-      </div>
-      <div className="headline">Discover and Connect with Skilled Developers.</div>
-      <div className="description">
-        <a href=" https://github.com/shyamtawli/devFind#how-to-add-your-profile-" target="_blank" rel="noreferrer">
-          Add your profile
-        </a>
+        <div className="sidebar-content">
+          <p>Discover and Connect with Skilled Developers.</p>
+          <a
+            href="https://github.com/shyamtawli/devFind#how-to-add-your-profile-"
+            target="_blank"
+            rel="noreferrer"
+            className="sidebar-cta"
+          >
+            Add your profile
+          </a>
+        </div>
       </div>
     </div>
   );
