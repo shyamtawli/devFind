d6e010385bc54a860ceab1f5d7726a5ea51b47cf
feelthatvib3 <88098862+feelthatvib3@users.noreply.github.com>
2023-07-25 20:57:11 +0200

    Update classNames' names and structure imports

--- src/components/NoResultFound/NoResultFound.jsx
+++ src/components/NoResultFound/NoResultFound.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import './NoResultFound.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFrown } from '@fortawesome/free-solid-svg-icons';
+
+import './NoResultFound.css';
+
 export default function NoResultFound() {
   return (
-    <div className="no-result-container">
+    <div className="no-result">
       <div className="no-result-card">
-        <div className="no-results">
-          <FontAwesomeIcon icon={faFrown} size="5x" />
-          <div className="text">
-            <h2>No Results Found</h2>
-            <p>We couldn't find any results for your search.</p>
-          </div>
+        <FontAwesomeIcon icon={faFrown} size="5x" />
+        <div className="no-result-card-content">
+          <h2>No results found</h2>
+          <p>We couldn't find any results for your search.</p>
         </div>
       </div>
     </div>
