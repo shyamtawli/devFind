1c458cb897b96cef43cf50a631568a70c93402e3
bismay <bismaypurkayastha5467@gmail.com>
2023-10-11 16:21:07 +0530

    changes made.

--- src/helpers/getSearchResult.js
+++ src/helpers/getSearchResult.js
@@ -1,18 +1,31 @@
 import getSearchKeywords from './sanitizeValue';
 
+const sanitizeString = (params) => {
+  return params.toLowerCase().trim();
+};
+
+const doesMatchExists = ({ searchKeyword, attribute }) => {
+  return attribute.includes(searchKeyword);
+};
+
+const doMatchingSkillsExist = ({ searchKeywords, skills }) => {
+  return searchKeywords.every((keyword) => skills.some((skill) => sanitizeString(skill) === keyword));
+};
+
 const getSearchResult = ({ searchValue, combinedData }) => {
   const searchKeywords = getSearchKeywords(searchValue);
-  const lowercaseSearch = searchKeywords.length === 0 ? searchValue.trim().toLowerCase() : searchKeywords[0];
+  const lowercaseSearch = searchKeywords.length === 0 ? sanitizeString(searchValue) : sanitizeString(searchKeywords[0]);
   const results = combinedData.filter((object) => {
     const { name, location, skills } = object;
 
-    const lowercaseName = name.toLowerCase();
-    const matchingLocations = searchKeywords.some((keyword) => location.toLowerCase().includes(keyword));
-    const matchingSkills = skills.some((skill) =>
-      searchKeywords.some((keyword) => skill.toLowerCase().includes(keyword)),
-    );
+    const sanitizedName = sanitizeString(name);
+    const sanitizedLocation = sanitizeString(location);
 
-    return matchingSkills || lowercaseName.includes(lowercaseSearch) || matchingLocations;
+    const matchingName = doesMatchExists({ searchKeyword: lowercaseSearch, attribute: sanitizedName });
+    const matchingLocation = doesMatchExists({ searchKeyword: lowercaseSearch, attribute: sanitizedLocation });
+    const matchingSkills = doMatchingSkillsExist({ searchKeywords, skills });
+
+    return matchingSkills || matchingName || matchingLocation;
   });
 
   return results;
