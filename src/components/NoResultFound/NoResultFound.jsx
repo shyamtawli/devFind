407bb4d0aadc7a847d3b610357acd86bbf89fe6c
nikcode9 <129772745+nikcode9@users.noreply.github.com>
2023-10-15 18:03:35 +0530

    Update NoResultFound.jsx

--- src/components/NoResultFound/NoResultFound.jsx
+++ src/components/NoResultFound/NoResultFound.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import './NoResultFound.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFrown } from '@fortawesome/free-solid-svg-icons';
+
 export default function NoResultFound() {
   return (
     <div className="no-result-container">
       <div className="no-result-card">
-        <div className="no-results">
-          <FontAwesomeIcon icon={faFrown} size="5x" />
+        <div className="no-result-content">
+          <FontAwesomeIcon icon={faFrown} size="5x" className="frown-icon" />
           <div className="text">
-            <h2>No Results Found</h2>
-            <p>We couldn't find any results for your search.</p>
+            <h2 className="no-result-heading">No Results Found</h2>
+            <p className="no-result-description">We couldn't find any results for your search.</p>
+            <p className="try-again">Please try a different search query or refine your filters.</p>
           </div>
+        </div>
+        <div className="suggestions">
+          <h3 className="suggestions-heading">Suggestions:</h3>
+          <ul className="suggestions-list">
+            <li>Check your spelling and try again.</li>
+            <li>Clear any applied filters.</li>
+            <li>Broaden your search criteria.</li>
+          </ul>
         </div>
       </div>
     </div>
