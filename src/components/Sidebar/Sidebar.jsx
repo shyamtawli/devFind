5bf06a91c799ef202ba1b39809726bfd49f138cf
Sean Lewertow <seanlewertow@Seans-MacBook-Air.local>
2024-02-09 03:17:22 -0500

    seanlewertow

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -17,41 +17,81 @@ function Sidebar() {
       setTheme('dark');
     }
   }
+
   return (
-    <div className="my-7 w-full border-r-2 border-borderSecondary px-7 font-spaceMono dark:border-borderColor md:h-[90vh] md:w-[23%]">
-      <div className="mb-2 flex h-12 items-center gap-2.5">
-        <div className="text-secondaryColor dark:text-white">
-          <FontAwesomeIcon icon={faCode} size="2xl" />
-        </div>
-        <a href="https://dev-find.vercel.app/">
-          <div className="flex text-[2rem] font-bold">
-            <p className="text-secondaryColor dark:text-white">dev</p>
-            <p className="text-textSecondary">Find</p>
+    <>
+      <style>
+        {`
+          /* Styles for light mode */
+          html:not(.dark) {
+            scrollbar-color: darkgrey lightgrey; /* for Firefox */
+            scrollbar-width: thin; /* for Firefox */
+          }
+          html:not(.dark) ::-webkit-scrollbar {
+            width: 12px; /* for Chrome, Safari, and Opera */
+          }
+          html:not(.dark) ::-webkit-scrollbar-track {
+            background: lightgrey;
+          }
+          html:not(.dark) ::-webkit-scrollbar-thumb {
+            background-color: darkgrey;
+            border-radius: 20px;
+            border: 3px solid lightgrey;
+          }
+
+          /* Styles for dark mode */
+          html.dark {
+            scrollbar-color: lightgrey darkgrey; /* for Firefox */
+            scrollbar-width: thin; /* for Firefox */
+          }
+          html.dark ::-webkit-scrollbar {
+            width: 12px; /* for Chrome, Safari, and Opera */
+          }
+          html.dark ::-webkit-scrollbar-track {
+            background: darkgrey;
+          }
+          html.dark ::-webkit-scrollbar-thumb {
+            background-color: lightgrey;
+            border-radius: 20px;
+            border: 3px solid darkgrey;
+          }
+        `}
+      </style>
+      <div className="my-7 w-full border-r-2 border-borderSecondary px-7 font-spaceMono dark:border-borderColor md:h-[90vh] md:w-[23%]">
+        <div className="mb-2 flex h-12 items-center gap-2.5">
+          <div className="text-secondaryColor dark:text-white">
+            <FontAwesomeIcon icon={faCode} size="2xl" />
           </div>
-        </a>
-        <div className="ml-auto">
-          <button
-            type="button"
-            className="h-10 w-10 cursor-pointer rounded-lg border-2 border-borderSecondary bg-white transition-all hover:border-textSecondary hover:text-textSecondary dark:border-borderColor dark:bg-textPrimary dark:text-white dark:hover:border-textSecondary dark:hover:text-textSecondary"
-            onClick={toggleTheme}
-          >
-            {theme === 'light' ? (
-              <FontAwesomeIcon icon={faMoon} fontSize="1rem" />
-            ) : (
-              <FontAwesomeIcon icon={faSun} fontSize="1rem" />
-            )}
-          </button>
+          <a href="https://dev-find.vercel.app/">
+            <div className="flex text-[2rem] font-bold">
+              <p className="text-secondaryColor dark:text-white">dev</p>
+              <p className="text-textSecondary">Find</p>
+            </div>
+          </a>
+          <div className="ml-auto">
+            <button
+              type="button"
+              className="h-10 w-10 cursor-pointer rounded-lg border-2 border-borderSecondary bg-white transition-all hover:border-textSecondary hover:text-textSecondary dark:border-borderColor dark:bg-textPrimary dark:text-white dark:hover:border-textSecondary dark:hover:text-textSecondary"
+              onClick={toggleTheme}
+            >
+              {theme === 'light' ? (
+                <FontAwesomeIcon icon={faMoon} fontSize="1rem" />
+              ) : (
+                <FontAwesomeIcon icon={faSun} fontSize="1rem" />
+              )}
+            </button>
+          </div>
+        </div>
+        <div className="text-secondaryColor dark:text-white">Discover and Connect with Skilled Developers.</div>
+        <div className="pt-5">
+          <a href="https://github.com/shyamtawli/devFind#how-to-add-your-profile-" target="_blank" rel="noreferrer">
+            <button className="inline-block cursor-pointer rounded-lg border-2 border-textSecondary bg-textSecondary px-[15px] py-1.5 text-center font-poppoins text-sm transition-all duration-500 hover:bg-transparent hover:text-textSecondary dark:text-white">
+              Add your profile
+            </button>
+          </a>
         </div>
       </div>
-      <div className="text-secondaryColor dark:text-white">Discover and Connect with Skilled Developers.</div>
-      <div className="pt-5">
-        <a href="https://github.com/shyamtawli/devFind#how-to-add-your-profile-" target="_blank" rel="noreferrer">
-          <button className="inline-block cursor-pointer rounded-lg border-2 border-textSecondary bg-textSecondary px-[15px] py-1.5 text-center font-poppoins text-sm transition-all duration-500 hover:bg-transparent hover:text-textSecondary dark:text-white">
-            Add your profile
-          </button>
-        </a>
-      </div>
-    </div>
+    </>
   );
 }
 
