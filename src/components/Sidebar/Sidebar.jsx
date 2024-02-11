6e47931794ac0b5ef69afbcd81a0bf690d676cd1
Vincent Toledo <toledov@ufl.edu>
2024-02-11 16:42:20 -0500

    add: Vincent Toledo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCode, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 function Sidebar() {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState('');
+
+  useEffect(() => {
+    // Check the initial theme mode when the component mounts
+    const htmlElement = document.documentElement;
+    const isDarkModeEnabled = htmlElement.classList.contains('dark');
+    // Set the initial theme mode in the state
+    setTheme(isDarkModeEnabled ? 'dark' : 'light');
+  }, []);
 
   function toggleTheme() {
     const htmlElement = document.documentElement;
@@ -11,14 +19,21 @@ function Sidebar() {
 
     if (isDarkModeEnabled) {
       htmlElement.classList.remove('dark');
+      htmlElement.classList.add('light');
       setTheme('light');
+      htmlElement.style.setProperty('--sidebar-background-color', 'rgb(70, 70, 70)');
+      htmlElement.style.setProperty('--track-color', 'rgb(200, 200, 200)');
     } else {
+      htmlElement.classList.remove('light');
       htmlElement.classList.add('dark');
       setTheme('dark');
+      htmlElement.style.setProperty('--sidebar-background-color', 'rgb(80, 80, 150)');
+      htmlElement.style.setProperty('--track-color', 'rgb(120, 120, 180)');
     }
   }
+
   return (
-    <div className="my-7 w-full border-r-2 border-borderSecondary px-7 font-spaceMono dark:border-borderColor md:h-[90vh] md:w-[23%]">
+    <div className="sidebar-content my-7 w-full border-r-2 border-borderSecondary px-7 font-spaceMono dark:border-borderColor md:h-[90vh] md:w-[23%]">
       <div className="mb-2 flex h-12 items-center gap-2.5">
         <div className="text-secondaryColor dark:text-white">
           <FontAwesomeIcon icon={faCode} size="2xl" />
