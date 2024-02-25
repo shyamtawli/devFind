da80bcc90d11a754c6b57d638e6a484c75b4a9d0
Swayam Vasavada <swayam.vasavada1505@gmail.com>
2024-02-25 22:21:46 +0530

    enchance-dynamic-profile-listing-#898

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import ErrorPage from './components/ErrorPage/ErrorPage';
 import NoResultFound from './components/NoResultFound/NoResultFound';
 import Pagination from './components/Pagination/Pagination';
 import './App.css';
-import filenames from './ProfilesList.json';
 
 function App() {
   const profilesRef = useRef();
@@ -20,124 +19,126 @@ function App() {
   const recordsPerPage = 20;
 
   const currentUrl = window.location.pathname;
-  useEffect(() => {
-    const fetchData = async (file) => {
-      try {
-        const response = await fetch(file);
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        return [];
-      }
-    };
 
-    const combineData = async () => {
-      setLoadingProfiles(true);
+  const [files, setFiles] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
       try {
-        const promises = filenames.map((file) => fetchData(`/data/${file}`));
-        const combinedData = await Promise.all(promises);
+        // Dynamically import the JSON files from the 'data' directory
+        const filesContext = require.context('./data', false, /\.json$/);
+        const files = await Promise.all(
+          filesContext.keys().map(async (key) => {
+            const fileName = key.replace(/^.*[\\/]/, ''); // Extract file name from path
+            const fileContent = await filesContext(key);
+            return { name: fileName, content: fileContent };
+          })
+        );
+        setFiles(files);
+        console.log(files);
+        const combinedData=[];
+        for (let i = 0; i < files.length; i++) {
+          const content = files[i].content;
+          combinedData.push(content);
+        }
+
         setCombinedData(combinedData);
         setShuffledProfiles(shuffleProfiles(combinedData));
       } catch (error) {
-        console.error('Error combining data:', error);
-        setCombinedData([]);
-        setShuffledProfiles([]);
+        console.error('Error fetching files:', error);
       }
-      setLoadingProfiles(false);
     };
-
-    combineData();
+    fetchData();
   }, []);
 
-  const shuffleProfiles = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  };
 
-  const handleSearch = (searchValue) => {
-    const lowercaseSearch = searchValue.toLowerCase().trim();
-    const results = combinedData.filter((object) => {
-      const lowercaseName = object.name.toLowerCase();
-      const lowercaseLocation = object.location.toLowerCase();
-      const matchingSkills = object.skills.filter((skill) => skill.toLowerCase().includes(lowercaseSearch));
-      return (
-        matchingSkills.length > 0 ||
-        lowercaseName.includes(lowercaseSearch) ||
-        lowercaseLocation.includes(lowercaseSearch)
-      );
-    });
+const shuffleProfiles = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
-    setSearching(true);
-    setProfiles(results);
-    setCurrentPage(1);
-  };
+const handleSearch = (searchValue) => {
+  const lowercaseSearch = searchValue.toLowerCase().trim();
+  const results = combinedData.filter((object) => {
+    const lowercaseName = object.name.toLowerCase();
+    const lowercaseLocation = object.location.toLowerCase();
+    const matchingSkills = object.skills.filter((skill) => skill.toLowerCase().includes(lowercaseSearch));
+    return (
+      matchingSkills.length > 0 ||
+      lowercaseName.includes(lowercaseSearch) ||
+      lowercaseLocation.includes(lowercaseSearch)
+    );
+  });
 
-  const handleNextPage = () => {
-    const totalPages = Math.ceil((searching ? profiles.length : combinedData.length) / recordsPerPage);
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
+  setSearching(true);
+  setProfiles(results);
+  setCurrentPage(1);
+};
 
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
+const handleNextPage = () => {
+  const totalPages = Math.ceil((searching ? profiles.length : combinedData.length) / recordsPerPage);
+  if (currentPage < totalPages) {
+    setCurrentPage(currentPage + 1);
+  }
+};
 
-  useEffect(() => {
-    profilesRef.current.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
-  }, [currentPage]);
+const handlePrevPage = () => {
+  if (currentPage > 1) {
+    setCurrentPage(currentPage - 1);
+  }
+};
 
-  const getPaginatedData = () => {
-    const data = searching ? profiles : shuffledProfiles;
-    const startIndex = (currentPage - 1) * recordsPerPage;
-    const endIndex = startIndex + recordsPerPage;
-    return data.slice(startIndex, endIndex);
-  };
+useEffect(() => {
+  profilesRef.current.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}, [currentPage]);
 
-  const renderProfiles = () => {
-    if (loadingProfiles) {
-      return (
-        <>
-          {Array(5)
-            .fill('profile-skeleton')
-            .map((item, index) => (
-              <ProfileSkeleton key={index} />
-            ))}
-        </>
-      );
-    }
-    const paginatedData = getPaginatedData();
-    return paginatedData.map((currentRecord, index) => <Profile data={currentRecord} key={index} />);
-  };
+const getPaginatedData = () => {
+  const data = searching ? profiles : shuffledProfiles;
+  const startIndex = (currentPage - 1) * recordsPerPage;
+  const endIndex = startIndex + recordsPerPage;
+  return data.slice(startIndex, endIndex);
+};
 
-  return currentUrl === '/' ? (
-    <div className="App flex flex-col bg-primaryColor dark:bg-secondaryColor md:flex-row">
-      <Sidebar />
-      <div className="w-full pl-5 pr-4 md:h-screen md:w-[77%] md:overflow-y-scroll md:py-7" ref={profilesRef}>
-        <Search onSearch={handleSearch} />
-        {profiles.length === 0 && searching ? <NoResultFound /> : renderProfiles()}
-        {profiles.length > 0 && (
-          <Pagination
-            currentPage={currentPage}
-            totalPages={Math.ceil((searching ? profiles.length : shuffledProfiles.length) / recordsPerPage)}
-            onNextPage={handleNextPage}
-            onPrevPage={handlePrevPage}
-          />
-        )}
-      </div>
+const renderProfiles = () => {
+  if (loadingProfiles) {
+    return (
+      <>
+        {Array(5)
+          .fill('profile-skeleton')
+          .map((item, index) => (
+            <ProfileSkeleton key={index} />
+          ))}
+      </>
+    );
+  }
+  const paginatedData = getPaginatedData();
+  return paginatedData.map((currentRecord, index) => <Profile data={currentRecord} key={index} />);
+};
+
+return currentUrl === '/' ? (
+  <div className="App flex flex-col bg-primaryColor dark:bg-secondaryColor md:flex-row">
+    <Sidebar />
+    <div className="w-full pl-5 pr-4 md:h-screen md:w-[77%] md:overflow-y-scroll md:py-7" ref={profilesRef}>
+      <Search onSearch={handleSearch} />
+      {profiles.length === 0 && searching ? <NoResultFound /> : renderProfiles()}
+      {profiles.length > 0 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={Math.ceil((searching ? profiles.length : shuffledProfiles.length) / recordsPerPage)}
+          onNextPage={handleNextPage}
+          onPrevPage={handlePrevPage}
+        />
+      )}
     </div>
-  ) : (
-    <ErrorPage />
-  );
+  </div>
+) : (
+  <ErrorPage />
+);
 }
 
 export default App;
