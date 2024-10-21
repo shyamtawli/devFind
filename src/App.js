0dbde5d98a20da002f978dabb18d07c4ae1703bc
SHASHI KUMAR KASTURI <59004150+kshashikumar@users.noreply.github.com>
2024-10-20 20:55:32 -0400

    Revert "add: kshashikumar"
    This reverts commit 0ae4e99ce3f2e5795c615a13e6688ed562239e48.

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ function App() {
       try {
         const response = await fetch(file);
         const data = await response.json();
-        console.log(data);
         return data;
       } catch (error) {
         console.error('Error fetching data:', error);
