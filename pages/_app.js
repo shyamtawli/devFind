7e8660c40b739f795b050af9e43bd55aea56ae80
oussama berhili <oussamaberhili190@gmail.com>
2025-02-17 12:20:04 +0100

    Refactor code to add ErrorBoundary component for better error handling

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
     <main className={inter.className}>
-      <Component {...pageProps} />
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
     </main>
   );
 }
